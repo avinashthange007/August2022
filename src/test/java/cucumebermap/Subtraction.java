143cd7242b39dfd4203dc31ca08e54fc58604a79
avinashthange007 <avinashthange007@gmail.com>
2022-08-23 00:17:41 +0530

    By Mr.David Work on develop

---
+++ src/test/java/cucumebermap/Subtraction.java
@@ -0,0 +1,38 @@
+package cucumebermap;
+
+public class Subtraction 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int a=100,c,d,e,f;
+		
+		int b=300;
+		
+		System.out.println("Number 1st is "+a);
+		
+		System.out.println("Number 1st is "+b);
+		
+		c=a*b;
+		
+		d=a-b;
+		
+		e=a+b;
+		
+		f=a*a;
+		
+		System.out.println("Multiplication of 2 Number is "+c);
+		
+		System.out.println("Subtration of 2 Number  is "+d);
+		
+		System.out.println("Addition of 2 Number  is "+e);
+		
+		System.out.println("Addition of 2 Number  is "+f);
+		
+		
+		System.out.println("DAvid Work");
+		
+	}
+
+}
