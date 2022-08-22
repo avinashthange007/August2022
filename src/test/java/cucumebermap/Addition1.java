6d417098daf7f5711f8660dd4526cfbc92ba6031
avinashthange007 <avinashthange007@gmail.com>
2022-08-22 22:39:22 +0530

    Mr.Avinash

---
+++ src/test/java/cucumebermap/Addition1.java
@@ -0,0 +1,28 @@
+package cucumebermap;
+
+public class Addition1 
+{
+
+	public static void main(String[] args) 
+	{
+			int a=25,c,d;
+			
+			int b=30;
+			
+			System.out.println("Number 1st is "+a);
+			
+			System.out.println("Number 1st is "+b);
+			
+			c=a*b;
+			
+			d=a-b;
+			
+			System.out.println("Multiplication of 2 Number is "+c);
+			
+			System.out.println("Subtration of 2 Number  is "+d);
+			
+			
+
+	}
+
+}
